becd83e390d68c82de0c57140d2ca2f8311de2e6
黄仲 <981379316@qq.com>
2016-10-24 21:36:35 +0800

    新增：删除和查询功能，整个demo具备增删该查功能了

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/demodb');
 var PersonSchema=new mongoose.Schema({
   name:String,
   age:String,
-  comment:String
+  comment:String,
+  visible:Boolean
 });
 //data modal
 mongoose.model('Person',PersonSchema);
 var Person=mongoose.model('Person');
-// var person=new Person();
-// person.name='wx';
-// person.age='22';
-// person.comment='hahah';
-// person.save();
 var app=express();
 app.use(express.static('./'));
 app.use(bodyParser.json());
@@ -26,29 +22,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.route('/list')
   .get(function(req,res){
       var kw=req.query.keyword;
-      console.log(req.query)
       // kw=JSON.parse(kw);
-      var query=kw?{$or:[{name:kw},{age:kw},{comment:kw}]}:{};
+      var query=kw?{$or:[{name:kw},{age:kw},{comment:kw}],$and:[{visible:true}]}:{visible:true};
       Person.find(query,function(err,docs){
         if(err){
           return
         }else{
-          res.send(docs);
+          var list=docs.map(function(item,ind,arr){
+            return {_id:item._id,name:item.name,age:item.age,comment:item.comment}
+          });
+          res.send(list.reverse());
           res.end();
         }
 
       });
   }).post(function(req,res){
     var data=req.body;
-    console.log(data.name)
     var newperson=new Person();
     newperson.name=data.name;
     newperson.age=data.age;
     newperson.comment=data.comment;
-    console.log(newperson);
+    newperson.visible=true;
     newperson.save();
     res.send(req.body)
     res.end('ok')
+  }).put(function(req,res){
+    var data=req.body;
+    Person.update({_id:data.id},{$set:data.value},function(err){
+        if(err){
+            return
+        }else{
+            res.end('ok');
+        }
+    })
+  }).delete(function(req,res){
+    var data=req.body;
+        Person.update({_id:data.id},{$set:{visible:false}},function(err){
+            if(err){
+                return
+            }else{
+                res.end('ok');
+            }
+        })
   })
 
 app.listen(18001,function(){
--- index.js
+++ index.js
@@ -2,40 +2,54 @@
 function $(selector){
   return document.querySelector(selector);
 }
-HTMLElement.prototype.val=function(){
-  return this.value;
+HTMLElement.prototype.val=function(v){
+  var value=v;
+  if(v||v===0||v===''){
+    this.value=v;
+  }else{
+    value=this.value
+  }
+  return value;
 }
 HTMLElement.prototype.html=function(v){
-  this.innerHTML=v;
-  return this
+  var value=v;
+  if(v||v===0||v===''){
+    this.innerHTML=v;
+  }else{
+    value=this.innerHTML;
+  }
+  return value
 };
 HTMLElement.prototype.removeAttr=function(attr){
   this.removeAttribute(attr);
   return this
+};
+HTMLElement.prototype.css=function(n,v){
+  this.style[n]=v;
+  return this
 }
 
 $('#form').onsubmit=function(e){
-  console.log(1)
   e.preventDefault();
   $('#name').disabled='disabled';
   $('#age').disabled='disabled';
   $('#comment').disabled='disabled';
   var data={
-    name:$('#name').val(),
-    age:$('#age').val(),
-    comment:$('#comment').val()
+    name:$('#name').val().trim(),
+    age:$('#age').val().trim(),
+    comment:$('#comment').val().trim()
   }
   var xhr=getXHR();
   xhr.open('POST','http://127.0.0.1:18001/list');
   xhr.setRequestHeader('Content-Type','application/json');
-  console.log(JSON.stringify(data))
   xhr.send(JSON.stringify(data));
   xhr.onreadystatechange=function(){
     if(xhr.readyState==4&&xhr.status==200){
-      $('#name').removeAttr('disabled');
-      $('#age').removeAttr('disabled');
-      $('#comment').removeAttr('disabled');
+      $('#name').removeAttr('disabled').val('');
+      $('#age').removeAttr('disabled').val('');
+      $('#comment').removeAttr('disabled').val('');
       alert('save successful');
+      init();
     }
   }
 }
@@ -50,6 +64,68 @@ $("#sbtn").onclick=function(){
     }
   }
 }
+$('b.close').onclick=function(){
+  $('.modal').css('display','none');
+}
+$('ul.list').onclick=function(e){
+  var target=e.target;
+  var li;
+  if(target.nodeName=='LI'||target.nodeName=='SPAN'){
+    li=target.nodeName=='LI'?target:target.parentNode;
+    liId=li.id;
+    $('#cname').val(li.querySelectorAll('.item')[0].html().slice(6));
+    $('#cage').val(li.querySelectorAll('.item')[1].html().slice(5));
+    $('#ccomment').val(li.querySelector('.com').html().slice(9))
+    $('.modal').css('display','block');
+  }
+  if(target.nodeName=='BUTTON'){
+    li=target.parentNode;
+    liId=li.id;
+    removeLi(liId);
+  }
+}
+var liId;
+//update
+$('#cform').onsubmit=function(e){
+  e.preventDefault();
+    $('#cname').disabled='disabled';
+    $('#cage').disabled='disabled';
+    $('#ccomment').disabled='disabled';
+    var data={
+      id:liId,
+      value:{
+        name:$('#cname').val().trim(),
+        age:$('#cage').val().trim(),
+        comment:$('#ccomment').val().trim()
+      }
+    }
+    var xhr=getXHR();
+    xhr.open('PUT','http://127.0.0.1:18001/list');
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(data));
+    xhr.onreadystatechange=function(){
+      if(xhr.readyState==4&&xhr.status==200){
+        $('#cname').removeAttr('disabled').val('');
+        $('#cage').removeAttr('disabled').val('');
+        $('#ccomment').removeAttr('disabled').val('');
+        $('.modal').css('display','none');
+        init();
+      }
+    }
+}
+//delete
+function removeLi(lId){
+  var xhr=getXHR();
+  xhr.open('delete','http://127.0.0.1:18001/list');
+  xhr.setRequestHeader('Content-Type','application/json');
+  xhr.send(JSON.stringify({"id":lId}));
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState==4&&xhr.status==200){
+      alert('success');
+      init();
+    }
+  }
+}
 init();
 function init(){
   var xhr=getXHR();
@@ -57,18 +133,19 @@ function init(){
   xhr.send(null);
   xhr.onreadystatechange=function(){
     if(xhr.readyState==4&&xhr.status==200){
-      console.log(xhr.responseText);
       renderList(JSON.parse(xhr.responseText));
     }
   }
 }
 function renderList(list){
   var List=list.map(function(item,ind,arr){
-    return ('<span class="item">name: '+item.name+'</span><span class="item">age: '+item.age+'</span><span class="com">comment: '+item.comment+'</span>')
-  })
-  List=List.join('</li><li>');
-  List='<li>'+List+'</li>';
-  $('.list').html(List);
+    return ('<li id="'+item._id+'"><span class="item">name: '+item.name+
+    '</span><span class="item">age: '
+    +item.age+'</span><span class="com">comment: '+item.comment+'</span>'
+    +'<button>delete</button></li>'
+    )
+  });
+  $('.list').html(List.join(""));
 }
 //get xhr
 function getXHR(){
